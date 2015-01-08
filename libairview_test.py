231ffd966507b2952b4179ab57464c02ad0f5660
Stephen Oliver <steve@infincia.com>
2015-01-08 12:15:31 -0500

    Merge fuzzer and test in to a single test program with multiple commands
    * Allows setting port on command line instead of auto detection
    * Allows setting length of commands to search for

--- libairview_test.py
+++ libairview_test.py
@@ -3,16 +3,26 @@
 from __future__ import print_function, division
 
 
-""" Airview2 "fuzzer" (more of a random tester, we're not looking for bugs just APIs)
+""" libairview test
 
-    Copyright 2015 Infincia LLC
+    Copyright 2014 Infincia LLC
     
     See LICENSE file for license information
     
-	Usage:  Install the libraries in requirements.txt, then run this script.
-    
-            It sends every possible 12 character [a-z] command to the device, 
-            checking for a response and recording the results as it goes.
+	Usage:  Install the libraries in requirements.txt, then just plug in your
+            Airview2 stick, and run ./test.py -h to see the command options.
+            
+            The only mandatory option is the port (-p /dev/tty*), and a command,
+            either 'scan' or 'fuzzer'.
+            
+            Note that this is simply an example of how to use the libairview 
+            library in your own code. Typically only 5-6 lines of code are needed
+            in order to use libairview for RSSI scanning.
+
+    Command: fuzzer
+
+            Sends every possible command to the device, up to 12 characters long
+            by default (-l 6 changes it to stop at 6, etc).
             
             Periodically results are printed to the terminal, and at the end of 
             testing or if the test is canceled, the final results are printed on 
@@ -23,6 +33,10 @@ from __future__ import print_function, division
             proper handling of the response, rather than a single response which
             is what we're testing for anyway to discover other commands.
             
+    Command: scan
+    
+            Demonstrates the RSSI scan stream function, no arguments are needed.
+
 """
 
 __author__ = 'Stephen Oliver'
@@ -30,10 +44,12 @@ __maintainer__ = 'Stephen Oliver <steve@infincia.com>'
 __version__ = '0.1-prerelease'
 __license__ = 'MIT'
 
-import platform
-import sys
+
+
 import logging
 import logging.handlers
+import sys
+import argparse
 import itertools
 
 
@@ -49,30 +65,30 @@ mainHandler.setFormatter(logging.Formatter('%(levelname)s %(asctime)s - %(module
 log.addHandler(mainHandler)
 
 
+def scan_callback(rssi_list=None):
+    log.info('RSSI levels received: %s', rssi_list)
 
-if __name__ == '__main__':
-    log.info('Starting Airview API Fuzzer')
-    system_name = platform.system()
-    logging.info('Running on: %s' % system_name)
-    if system_name == 'Linux':
-        port = '/dev/ttyACM0' # arbitrary, need a way to detect/configure
-    elif system_name == 'Darwin': # OS X
-        port = '/dev/tty.usbmodem411' # this seems arbitrary too, but consistent
-    else:
-        """
-                Note: Need to find out what the device name will be on FreeBSD 
-                and others, but this is just the command discovery automator, the 
-                library will still work as-is everywhere that PySerial does.
 
-        """
-        log.error('Unknown platform')
-        sys.exit(1)
+def scan(args):
+    log.info('Starting Airview RSSI scan')
     try:
-        log.info('Beginning API command search')
+        airview.start_background_scan(callback=scan_callback)
+    except KeyboardInterrupt as e:
+        log.info('Cancelling scan')
+        airview.stop_background_scan()
+    finally:
+        log.info('Exiting')
+
+
+
+def fuzzer(args):
+    log.info('Starting Airview API Fuzzer')
+    try:
+        log.info('Beginning API search for commands up to %d letters', args.length)
         found_commands = {}
         check_count = 0
-        airview.connect(port=port)
-        for length in xrange(12): # limit to 12 for now, most known commands are 4 bytes
+
+        for length in xrange(args.length):
             for command in itertools.product('abcdefghijklmnopqrstuvwxyz', repeat=length):
                 check_count = check_count + 1
                 command_string = ''.join(command)
@@ -94,6 +110,33 @@ if __name__ == '__main__':
     finally:
         log.info('Checked %d commands total and got %d valid responses: %s', check_count, len(found_commands), found_commands)
         log.info('Exiting')
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(description='Airview2 test program')
+    arg_parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1-prerelease')
+    arg_parser.add_argument('-p', '--port', help='The serial port of the Airview2 device (/dev/tty*)', required=True)
+
+    subparsers = arg_parser.add_subparsers()
+    parser_fuzzer = subparsers.add_parser('fuzzer')
+    parser_fuzzer.add_argument('-l', '--length', type=int, default=6, help='The length of command strings to check (default: 6)')
+    parser_fuzzer.set_defaults(func=fuzzer)
+
+    parser_scan = subparsers.add_parser('scan')
+    parser_scan.set_defaults(func=scan)
+
+    args = arg_parser.parse_args()
+
+    connected = airview.connect(port=args.port)
+    if not connected:
+        log.error('Port already in use')
+        sys.exit(1)
+    log.info('Initializing device')
+    initialized = airview.initialize()
+    if initialized:
+        args.func(args)
+    else:
+        log.info('Initialization failed!')
 
 
 
