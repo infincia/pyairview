034c02a37630c6111a507e62fc4ad5269b60d5ed
Stephen Oliver <steve@infincia.com>
2015-01-09 19:08:53 -0500

    Switch to standard PEP 440 version string

--- libairview.py
+++ libairview.py
@@ -40,7 +40,7 @@ from __future__ import print_function
 
 __author__ = 'Stephen Oliver'
 __maintainer__ = 'Stephen Oliver <steve@infincia.com>'
-__version__ = '0.1-prerelease'
+__version__ = '0.1a0'
 __license__ = 'MIT'
 
 
